670332609648ce2dcbf506f54c672ccce77d1dc2
Amit Samanta <amitsamanta002@gmail.com>
2018-11-10 00:29:49 +0530

    Circular linkedlist added

--- src/org/amit/ds/Linkedlist/circular/CircularLinkedlist.java
+++ src/org/amit/ds/Linkedlist/circular/CircularLinkedlist.java
@@ -7,24 +7,71 @@ public class CircularLinkedlist {
 //Insert element in circular list at the beginning...
 	public void insertNode(int data){
 		if(tail==null){
-			Node newnode = new Node(data);
-			tail = newnode;
-			tail.setNextNode(tail);
+			Node newNode = new Node(data);
+			tail = newNode;
+			tail.setNextNode(newNode);
 		}
 		else{
-			Node newnode = new Node(data);
-			newnode.setNextNode(tail.getNextNode());
-			tail.setNextNode(newnode);
+			Node newNode = new Node(data);
+			newNode.setNextNode(tail.getNextNode());
+			tail.setNextNode(newNode);
 		}
 	}
 
 //Display elements on Circular list...
 	public void displayList(){
-		Node current = tail.getNextNode();
-		while(current.equals(tail) == false){
-			System.out.println("Elements:"+current.getData());
+		Node current = tail;
+		if(tail == null)
+			System.out.println("List is empty.");
+
+		System.out.println("Elements:"+current.getData());
+		current = current.getNextNode();
+		while(current.equals(tail) == false) {
+			System.out.println("Elements:" + current.getData());
 			current = current.getNextNode();
 		}
+	}
+	//Get counts of the list.
+	public int getCount(){
+		if(tail == null)
+			return 0;
+		else{
+			Node current = tail;
+			int count = 1;
+			while(current.getNextNode().equals(tail) == false){
+				count++;
+				current = current.getNextNode();
+			}
+			return count;
+		}
+	}
+	//Delete of a node in circular linked list.
+
+	public boolean deleteNode(int data){
+		boolean flag = false;
+		//This if block will execute if list has only one element...
+		if(tail.getData() == data && tail.getNextNode() == tail){
+			tail = null;
+			flag= true;
+		}
+		else{
+			Node current = tail;
+			while(current.getNextNode().equals(tail) == false){
+				if(current.getNextNode().getData() == data){
+					current.setNextNode(current.getNextNode().getNextNode()); // removing link for middle node whose data match with delete node.
+					flag = true;
+				}
+				current = current.getNextNode();
+			}
+			//This part of of logic execute when tail node has to be deleted and list has more than one element;
+			if(tail.getData() == data){
+				current.setNextNode(tail.getNextNode());
+				tail = tail.getNextNode();
+				flag = true;
+			}
+
+		}
+		return flag;
 	}
 	
 }
--- src/org/amit/ds/Linkedlist/circular/CircularListDemo.java
+++ src/org/amit/ds/Linkedlist/circular/CircularListDemo.java
@@ -12,15 +12,22 @@ public class CircularListDemo {
 	public static void main(String[] args) {
 		CircularLinkedlist circularList = new CircularLinkedlist();
 		circularList.insertNode(10);
-	/*	circularList.insertNode(80);
+		circularList.insertNode(80);
 		circularList.insertNode(70);
 		circularList.insertNode(60);
 		circularList.insertNode(90);
-		circularList.insertNode(40); */
+		circularList.insertNode(40);
 		
 //Display elements of Circular Linked list...
 		circularList.displayList();
-		
+		//Getting no of node in circular linked list..
+		System.out.println("Total node count before Delete ="+circularList.getCount());
+
+		//Deleting only one element of the list.
+		System.out.println("Delete successful:"+circularList.deleteNode(10));
+
+		circularList.displayList();
+		System.out.println("Total node count after delete ="+circularList.getCount());
 		
 	}
 
