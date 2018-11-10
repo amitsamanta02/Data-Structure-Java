389694d998976881108192325c662087e265942f
Amit Samanta <amitsamanta002@gmail.com>
2018-11-11 00:01:24 +0530

    All functions for double linked list.

--- src/org/amit/ds/Linkedlist/doubley/DoubleLinkedList.java
+++ src/org/amit/ds/Linkedlist/doubley/DoubleLinkedList.java
@@ -7,7 +7,7 @@ package org.amit.ds.Linkedlist.doubley;
 public class DoubleLinkedList {
     private Node head;
     private Node tail;
-    private int count = 0;
+    public int count = 0;
 
     /*
     Add logic to add elements in double linked list at the beginning of the list.
@@ -24,6 +24,64 @@ public class DoubleLinkedList {
             head = newNode;
         }
         count++;
+    }
+
+    /*
+    Add node at the end of the list..
+     */
+
+    public void insertAtEnd(int data){
+        Node newNode = new Node(data);
+        if(tail!=null){
+            newNode.setPrevNode(tail);
+            tail.setNextNode(newNode);
+            tail=newNode;
+        }
+        if(head==null && tail==null){
+            head = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+    /*
+    Display element from last to first. <Tail to head>
+     */
+    public void displayFromlast(){
+        Node current = tail;
+        while(current!=head){
+            System.out.println("Node:"+current.getData());
+            current = current.getPrevNode();
+        }
+        //Display head element
+        System.out.println("Node:"+head.getData());
+    }
+    /*
+    DELETE A NODE IN DOUBLE LINKED LIST.
+    */
+    public boolean deleteNode(int data){
+        boolean flag = false;
+        Node current = head;
+        //logic to delete head of the list...
+        if(current.getData() == data){
+            head = current.getNextNode();
+            current.getNextNode().setPrevNode(null);
+            flag = true;
+            count --;
+        }
+        //other than head, delete logic for other node..
+        else
+        {
+            while(current.getNextNode() != tail) {
+                if (current.getNextNode().getData() == data) {
+                    current.getNextNode().getNextNode().setPrevNode(current);
+                    current.setNextNode(current.getNextNode().getNextNode());
+                    flag = true;
+                    count --;
+                }
+                current = current.getNextNode();
+            }
+        }
+        return flag;
     }
     /*
     Add logic to display elements on Double linked list...
--- src/org/amit/ds/Linkedlist/doubley/DoubleLinkedlistDemo.java
+++ src/org/amit/ds/Linkedlist/doubley/DoubleLinkedlistDemo.java
@@ -8,13 +8,23 @@ public class DoubleLinkedlistDemo {
     public static void main(String[] args){
         DoubleLinkedList dlist = new DoubleLinkedList();
         //Add node to the list at the beginning...
-        dlist.insertAtBeginning(10);
+       /* dlist.insertAtBeginning(10);
         dlist.insertAtBeginning(90);
         dlist.insertAtBeginning(30);
         dlist.insertAtBeginning(40);
-        dlist.insertAtBeginning(20);
+        dlist.insertAtBeginning(20);*/
 
         //display elements of the list...
+        dlist.insertAtEnd(40);
+        dlist.insertAtEnd(30);
+        dlist.insertAtEnd(50);
+        dlist.insertAtEnd(10);
+        dlist.insertAtEnd(20);
+
+
         dlist.displayNode();
+        dlist.deleteNode(40);
+        dlist.displayFromlast();
+        System.out.println("Total Node Count ="+dlist.count);
     }
 }
